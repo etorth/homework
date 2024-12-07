79eda495d7d930d670bf6e39aef53afb461463fb
etorth <anhonghe@gmail.com>
2024-12-06 22:11:58 -0800

    f

--- main.cpp
+++ main.cpp
@@ -3,30 +3,32 @@
 #include <iostream>
 #include <algorithm>
 
+int random_pick(int min, int max)
+{
+    return std::rand() % (max - min + 1) + min;
+}
+
 std::string f3()
 {
-    const int sum = (std::rand() % 35) + 15;
-
-    const int p1 = std::clamp(std::rand() % (sum     ), 6, 33);
-    const int p2 = std::clamp(std::rand() % (sum - p1), 7, 42);
-
-    const int p3 = std::max<int>(sum - p1 - p2, 8);
+    const int p1 = random_pick(25, 29);
+    const int p2 = random_pick(25, 29);
+    const int p3 = random_pick(25, 29);
 
     char buf[128];
     switch(std::rand() % 3){
         case 0:
             {
-                std::sprintf(buf, "%2d + %2d + %2d = ____", p1, p2, p3);
+                std::sprintf(buf, "%2d + %2d + %2d =", p1, p2, p3);
                 break;
             }
         case 1:
             {
-                std::sprintf(buf, "%2d + %2d - %2d = ____", p1 + p2, p3, p1);
+                std::sprintf(buf, "%2d + %2d - %2d =", p1 + p2, p3, p1);
                 break;
             }
         default:
             {
-                std::sprintf(buf, "%2d - %2d + %2d = ____", p1 + p2, p1, p3);
+                std::sprintf(buf, "%2d - %2d + %2d =", p1 + p2, p1, p3);
                 break;
             }
     }
@@ -35,16 +37,15 @@ std::string f3()
 
 std::string f2()
 {
-    const int sum = (std::rand() % 35) + 15;
-    const int p1 = std::clamp(std::rand() % sum, 7, 42);
+    const int p1 = random_pick(25, 29);
+    const int p2 = random_pick(25, 29);
 
     char buf[128];
     if(std::rand() % 2){
-        const auto p2 = sum - p1;
-        std::sprintf(buf, "%2d + %2d = ____", p1, p2);
+        std::sprintf(buf, "%2d + %2d =", p1, p2);
     }
     else{
-        std::sprintf(buf, "%2d - %2d = ____", sum, p1);
+        std::sprintf(buf, "%2d - %2d =", p1 + p2, p2);
     }
 
     return buf;
@@ -59,20 +60,12 @@ int main(int, char *argv[])
 
     if(digits == 2){
         for(int j = 0; j < lines; ++j){
-            for(int i = 0; i < 7; ++i){
-                std::cout << f2() << "          " << f2() << "          " << f2() << std::endl;
-            }
-            std::cout << std::endl;
-            std::cout << std::endl;
+            std::cout << f2() << "          " << f2() << "          " << f2() << std::endl;
         }
     }
     else if(digits == 3){
         for(int j = 0; j < lines; ++j){
-            for(int i = 0; i < 7; ++i){
-                std::cout << f3() << "          " << f3() << std::endl;
-            }
-            std::cout << std::endl;
-            std::cout << std::endl;
+            std::cout << f3() << "          " << f3() << std::endl;
         }
     }
     return 0;
